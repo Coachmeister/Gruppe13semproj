36ff3a496a7f04d6e3d267b10b7b6d7eb59a7afa
Jones_lund <jonas.lund@hotmail.dk>
2018-11-27 13:05:05 +0100

    Hele lortet

---
+++ Game1/src/game1/levelData.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game1;
+
+/**
+ *
+ * @author Jonas
+ */
+public class levelData {
+
+    public static final String[] LEVEL1 = new String[]{
+    "000000000000000000000",
+    "000000000000000000000",
+    "000000000000000000000",
+    "000000000000000000000",
+    "000000000000000000000",
+    "000000000000000000000",
+    "000000000000000000000",
+    "000000000000000000000",
+    "000000000000000000000",
+    "000000000000000000000",
+    "000000000000000000000",
+    "111111111111111111111"
+    
+    };
+}
