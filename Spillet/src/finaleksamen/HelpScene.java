49dd82100e350deeb01df06cbda9e534ac3c509c
Jones_lund <jonas.lund@hotmail.dk>
2018-12-07 09:45:43 +0100

    Updated help menu

--- Spillet/src/finaleksamen/HelpScene.java
+++ Spillet/src/finaleksamen/HelpScene.java
@@ -20,11 +20,14 @@ public class HelpScene {
     
     public Pane scene(Stage primaryStage, Scene GoBackScene){
         
-        Label label1= new Label("Help info here");
+        Label label1= new Label("Welcome to the help menu! Here you can find information about the game.");
+        Label label2 = new Label("Player movement: WASD.");
+        Label label3 = new Label("Interaction with rooms, items and triggers: E");
+      
         Button button1= new Button("Back");
         button1.setOnAction(e -> primaryStage.setScene(GoBackScene));   
         VBox layout_help = new VBox(20);     
-        layout_help.getChildren().addAll(label1, button1);
+        layout_help.getChildren().addAll(label1, button1,label2, label3);
         
         return layout_help;
         
